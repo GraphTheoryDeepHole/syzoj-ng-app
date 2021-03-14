de4d693db6c6459260d257cc04e17144545c0487
xsun2001 <xcx14@outlook.com>
2021-03-14 08:34:05 +0800

    feat(graph): add forward list input

--- src/pages/graph-editor/input/methods/index.ts
+++ src/pages/graph-editor/input/methods/index.ts
@@ -2,12 +2,14 @@ import RandomGraphComponent from "@/pages/graph-editor/input/methods/RandomGraph
 import AdjacencyMatrixComponent from "@/pages/graph-editor/input/methods/AdjacencyMatrixComponent";
 import IncidenceMatrixComponent from "@/pages/graph-editor/input/methods/IncidenceMatrixComponent";
 import EdgeListComponent from "@/pages/graph-editor/input/methods/EdgeListComponent";
+import ForwardListComponent from "@/pages/graph-editor/input/methods/ForwardListComponent";
 
 const methods = new Map([
   ["random", RandomGraphComponent],
   ["adjmat", AdjacencyMatrixComponent],
   ["incmat", IncidenceMatrixComponent],
-  ["edge_list", EdgeListComponent]
+  ["edge_list", EdgeListComponent],
+  ["fwd_list", ForwardListComponent]
 ]);
 
 export default methods;
